f26458f1eb5d5523b3e11aec6378182821c55646
azizaelhamyani <2006091000367@ofppt.edu.ma>
2025-09-17 14:40:25 +0100

    aziz

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
-console.log("La somme est : " + somme);
-console.log("La somme est : " + somme);
+// let nombre1 = 10;
+// let nombre2 = 5;
+// let somme1 = nombre1 + nombre2;
+// console.log("La somme est : " + somme1);
+// console.log("La somme est : " + somme1);
+// console.log("La somme est : " + somme1);
+// function addition(a, b) {
+//     return a + b;
+//   }
+  
+//   let somme2= addition(8, 5);
+//   console.log("La sommeconsole.log est : " + somme2);
+//CHAPTRE 8
+function addition(a, b) {
+    return a + b;
+  }
+  
+  let somme = addition(8, 5);
+  console.log("La somme est : " + somme);
+  
