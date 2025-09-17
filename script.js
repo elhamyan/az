92519ceaad50b7782b1bb5a7b318a2f8ec2d4ec9
azizaelhamyani <2006091000367@ofppt.edu.ma>
2025-09-17 15:23:04 +0100

    pare

--- script.js
+++ script.js
@@ -18,13 +18,18 @@
 //   let somme = addition(8, 5);
 //   console.log("La somme est : " + somme);
   //chaptre 9
-  let age = 20;
+//   let age = 20;
 
-  if (age < 18) {
-    console.log("Vous êtes mineur.");
-  } else if (age === 18) {
-    console.log("Vous venez d’atteindre la majorité !");
-  } else {
-    console.log("Vous êtes majeur.");
+//   if (age < 18) {
+//     console.log("Vous êtes mineur.");
+//   } else if (age === 18) {
+//     console.log("Vous venez d’atteindre la majorité !");
+//   } else {
+//     console.log("Vous êtes majeur.");
+//   }
+  //chaptre 10
+  for (let i = 1; i <= 10; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
   }
   
